b2d31343184c3b10006dcc0afa0d2668fddfa63e
DomanTom07 <tamasdoman7@citromail.hu>
2022-06-14 09:55:06 +0200

    HTML kész

--- src/calc.php
+++ src/calc.php
@@ -7,3 +7,12 @@ Date: 2022-06-14
 Github: https://github.com/DomanTom07/
 Licenc: GNU GPL
 -->
+
+<?php
+
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/nav.html');
+echo file_get_contents('templates/calc.html');
+echo file_get_contents('templates/foot.html');
+
+?>
